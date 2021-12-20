bd383d9e9e1a12a87ae813ab1b41ccd888d2cebe
Leonardo Pescetti <93586306+leopes99@users.noreply.github.com>
2021-12-20 12:13:54 +0100

    Add files via upload

---
+++ DemoProgetto/src/main/java/it/progettoApp/DemoProgetto/DemoProgettoApplication.java
@@ -0,0 +1,13 @@
+package it.progettoApp.DemoProgetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProgettoApplication.class, args);
+	}
+
+}
---
+++ DemoProgetto/src/test/java/it/progettoApp/DemoProgetto/DemoProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.progettoApp.DemoProgetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

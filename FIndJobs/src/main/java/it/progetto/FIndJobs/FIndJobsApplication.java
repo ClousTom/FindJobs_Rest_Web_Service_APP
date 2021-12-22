2f24437874f2b2cc9cf3796b68572d96e277bf13
Leonardo Pescetti <93586306+leopes99@users.noreply.github.com>
2021-12-22 12:21:48 +0100

    Add files via upload

---
+++ FIndJobs/src/main/java/it/progetto/FIndJobs/FIndJobsApplication.java
@@ -0,0 +1,13 @@
+package it.progetto.FIndJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIndJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIndJobsApplication.class, args);
+	}
+
+}
---
+++ FIndJobs/src/test/java/it/progetto/FIndJobs/FIndJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.progetto.FIndJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FIndJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

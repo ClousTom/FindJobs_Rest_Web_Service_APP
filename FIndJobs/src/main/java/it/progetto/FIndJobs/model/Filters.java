a1bf830ac3315177294f9e17b0f70b024bfcdbd6
S1078475 <s1078475@studenti.univpm.it>
2022-01-04 13:10:56 +0100

    parseData and Filters

---
+++ FIndJobs/src/main/java/it/progetto/FIndJobs/model/Filters.java
@@ -0,0 +1,40 @@
+package it.progetto.FIndJobs.model;
+import java.util.Vector;
+
+public class Filters {
+
+	public static void RemoteJobs (Lavori[] x){
+		for(int i=0;i<x.length;i++) {
+			if(x[i].isRemote()) {
+				System.out.println(x[i].isRemote()+" "+i);
+			}
+		}
+	}
+	
+	public static void NoRemoteJobs (Lavori[] x){
+		for(int i=0;i<x.length;i++) {
+			if(!x[i].isRemote()) {
+				System.out.println(x[i].isRemote()+" "+i);
+			}
+		}
+	}
+	
+	public static void CitySearch (Lavori[] x){
+		String city="Austin";
+		for(int i=0;i<x.length;i++) {
+			if(x[i].isCity(city)) {
+				System.out.println(x[i].getLocation()+" "+i);
+			}
+		}
+	}
+	
+	public static void LanguageSearch (Lavori[] x){
+		String lang="javascript";
+		for(int i=0;i<x.length;i++) {
+			if(x[i].isLang(lang)) {
+				System.out.println(i+" "+lang);
+			}
+		}
+		
+	}
+}
